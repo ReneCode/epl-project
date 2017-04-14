721d511622fcfb9b2cea0d8a1b08a522a16c6dd5
rene_mbp <harryloveslearning@googlemail.com>
2017-04-14 15:17:25 +0200

    first project overview

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EplProjectPage } from './app.po';
+
+describe('epl-project App', () => {
+  let page: EplProjectPage;
+
+  beforeEach(() => {
+    page = new EplProjectPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
